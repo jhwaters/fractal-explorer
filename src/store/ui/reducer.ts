4397e36891b98c4ec09cbdddc6b7a1e68650a0e2
JW <>
2020-04-02 16:39:09 -0400

    add return if no update

--- src/store/ui/reducer.ts
+++ src/store/ui/reducer.ts
@@ -33,23 +33,23 @@ const initialState: State = ({
 export default function(state: State=initialState, action: Action) {
   switch(action.type) {
     case UPDATE:
-      return {...state, ...action.payload}
+      return {...state, ...action.payload};
     case SET_NAV:
       if (action.payload === Nav.Gallery) {
-        return {...state, nav: action.payload, galleryBadge: 0}
+        return {...state, nav: action.payload, galleryBadge: 0};
       } else {
-        return {...state, nav: action.payload}
+        return {...state, nav: action.payload};
       }
     case SET_MODAL:
-      return {...state, modal: action.payload}
+      return {...state, modal: action.payload};
     case SET_CANVAS_ACTION:
       if (state.nav === Nav.Capture && (action.payload === CanvasAction.Draw || action.payload === CanvasAction.Color)) {
-        return {...state, canvasAction: action.payload, nav: Nav.Explore}
+        return {...state, canvasAction: action.payload, nav: Nav.Explore};
       } else {
-        return {...state, canvasAction: action.payload}
+        return {...state, canvasAction: action.payload};
       }
     case SET_STRETCH:
-      return {...state, canvasStretch: action.payload}
+      return {...state, canvasStretch: action.payload};
     case ADD_COLOR_SCHEME:
       return {
         ...state, 
@@ -57,9 +57,9 @@ export default function(state: State=initialState, action: Action) {
           ...state.colorSchemeList,
           [action.payload.name]: action.payload.scheme,
         },
-      }
+      };
     case ADD_TO_GALLERY:
-      return {...state, galleryBadge: state.galleryBadge+1}
+      return {...state, galleryBadge: state.galleryBadge+1};
     case UPDATE_FRACTAL:
       if (action.payload && !(state.colorSchemeList[action.payload.color.schemeName])) {
         return {
@@ -68,9 +68,10 @@ export default function(state: State=initialState, action: Action) {
             ...state.colorSchemeList,
             [action.payload.color.schemeName]: action.payload.color.scheme,
           },
-        }
+        };
       }
+      return state;
     default:
-      return state
+      return state;
   }
 }
